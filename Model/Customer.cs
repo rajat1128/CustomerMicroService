3f9f0db4fb934d817f4a2a70fbcbb205545f2f96
rajat1128 <63598934+rajat1128@users.noreply.github.com>
2020-05-29 19:40:08 +0530

    Add project files.

---
+++ Model/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerMicroService.Model
+{
+    [Table("Customer_Order")]
+    public class Customer
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; }
+        public string StreetAddress { get; set; }
+        public int Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string TN { get; set; }
+        public string CreatedBy { get; set; }
+        public string TNType { get; set; }
+        public string Operation { get; set; }
+        public string RequestData { get; set; }
+    }
+}
---
+++ Model/CustomerContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerMicroService.Model
+{
+    public class CustomerContext : DbContext
+    {
+        public DbSet<Customer> Customer { get; set; }
+
+        public CustomerContext(DbContextOptions<CustomerContext> options) : base(options)
+        {
+
+        }
+    }
+}
